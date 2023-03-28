cb3d1fbd6394a493cf2253bbdc38ecce4d697646
wxt9861 <34514130+wxt9861@users.noreply.github.com>
2023-03-28 15:59:05 -0400

    chore: Bump version

--- custom_components/ynab/const.py
+++ custom_components/ynab/const.py
@@ -4,7 +4,7 @@ DOMAIN_DATA = f"{DOMAIN}_data"
 
 PLATFORMS = ["sensor"]
 REQUIRED_FILES = ["const.py", "manifest.json", "sensor.py"]
-VERSION = "0.1.13"
+VERSION = "0.2.0"
 ISSUE_URL = "https://github.com/wxt9861/ynab/issues"
 
 STARTUP = """
